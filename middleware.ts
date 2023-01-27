0d5ad5efe6b8197d3449b336e42e58d7f27fca18
Achintha Ikiriwatte <achintha.ikiriwatte@outlook.com>
2023-01-27 20:00:09 +0530

    added log

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { get } from '@vercel/edge-config';
 import { NextRequest } from 'next/server';
 
-export const config = { matcher: ['/'] };
+export const config = { matcher: '/' };
 
 export async function middleware(req: NextRequest) {
   const edgeConfig = await get('isInMaintenanceMode');
