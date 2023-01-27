1c743491a43fb1c501f1774fad4a8b24d090f3f5
Achintha Ikiriwatte <achintha.ikiriwatte@outlook.com>
2023-01-27 19:56:39 +0530

    cookie added

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,12 @@
-import { NextResponse } from 'next/server';
 import { get } from '@vercel/edge-config';
+import { NextRequest } from 'next/server';
 
-export const config = { matcher: '/' };
+export const config = { matcher: ['/'] };
 
-export async function middleware() {
+export async function middleware(req: NextRequest) {
   const edgeConfig = await get('isInMaintenanceMode');
   // NextResponse.json requires at least Next v13.1 or
   // enabling experimental.allowMiddlewareResponseBody in next.config.js
-  return NextResponse.json(edgeConfig);
+//   return NextResponse.json(edgeConfig);
+    req.cookies.set("cookie", edgeConfig)
 }
