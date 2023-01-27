6ff6b4692ce81592b7b34f7a36218ee0d8186ce7
Achintha Ikiriwatte <achintha.ikiriwatte@outlook.com>
2023-01-27 19:53:41 +0530

    fixed edge

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,11 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { getAll } from '@vercel/edge-config';
+import { NextResponse } from 'next/server';
+import { get } from '@vercel/edge-config';
 
-export const config = { matcher: ['/'] };
+export const config = { matcher: '/' };
 
-export async function middleware(req: NextRequest) {
-
-    if (!process.env.EDGE_CONFIG) {
-        console.error("No config found")
-        // return NextResponse.rewrite(req.nextUrl)
-    }
-
-    try {
-        // Check whether the maintenance page should be shown
-        const jsonConfig = await getAll()
-
-        // If is in maintenance mode, point the url pathname to the maintenance page
-        if (jsonConfig) {
-            console.log(jsonConfig)
-            req.cookies.set("config", jsonConfig)
-            // Rewrite to the url
-            return NextResponse
-        }
-    } catch (error) {
-        // show the default page if EDGE_CONFIG env var is missing,
-        // but log the error to the console
-        console.error(error)
-    }
+export async function middleware() {
+  const edgeConfig = await get('isInMaintenanceMode');
+  // NextResponse.json requires at least Next v13.1 or
+  // enabling experimental.allowMiddlewareResponseBody in next.config.js
+  return NextResponse.json(edgeConfig);
 }
---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    /* config options here */
+    experimental: {
+        allowMiddlewareResponseBody: true
+    }
+  }
+  
+  export default nextConfig
